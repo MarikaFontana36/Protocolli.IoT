8258ede8d88b319f976fa5ae2809341174735390
davidescapolan <84379592+davidescapolan@users.noreply.github.com>
2021-11-22 16:18:37 +0100

    Add files via upload

---
+++ Server/ITS.DIQU.FontanaScapolan.ServerDrone/ITS.DIQU.FontanaScapolan.ServerDrone.ApplicationCore/Entities/Drone.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ITS.DIQU.FontanaScapolan.ServerDrone.ApplicationCore.Entities
+{
+    public class Drone : Entity<int>
+    {
+        public DateTime Date { get; set; }
+
+        public string Position { get; set; }
+
+        public double Speed { get; set; }
+
+        public int BatteryLevel { get; set; }
+    }
+}
---
+++ Server/ITS.DIQU.FontanaScapolan.ServerDrone/ITS.DIQU.FontanaScapolan.ServerDrone.ApplicationCore/Interfaces/Services/IDronesService.cs
@@ -0,0 +1,15 @@
+﻿using ITS.DIQU.FontanaScapolan.ServerDrone.ApplicationCore.Entities;
+using System.Collections.Generic;
+
+namespace ITS.DIQU.FontanaScapolan.ServerDrone.ApplicationCore.Interfaces.Services
+{
+    public interface IDronesService
+    {
+        IEnumerable<Drone> GetAllDrones();
+        void Delete(int id);
+        public Drone GetByDroneId(int Id);
+        long GetCount();
+        void Insert(Drone model);
+        void Update(Drone model);
+    }
+}
