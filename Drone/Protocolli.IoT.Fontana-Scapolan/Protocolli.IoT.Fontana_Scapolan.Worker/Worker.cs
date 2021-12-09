e19b1901ff7d79073f22bd98aa953e36d85525ee
davide.scapolan <davide.scapolan@stud.tecnicosuperiorekennedy.it>
2021-12-09 10:09:49 +0100

    Allineamento con branch MQTT

--- Drone/Protocolli.IoT.Fontana-Scapolan/Protocolli.IoT.Fontana_Scapolan.Worker/Worker.cs
+++ Drone/Protocolli.IoT.Fontana-Scapolan/Protocolli.IoT.Fontana_Scapolan.Worker/Worker.cs
@@ -1,11 +1,13 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Protocolli.IoT.Fontana_Scapolan.Worker.Models;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Subscribing;
 using System;
 using System.Net;
 using System.Net.Http;
 using System.Text;
-using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -15,6 +17,8 @@ namespace Protocolli.IoT.Fontana_Scapolan.Worker
     {
         private readonly ILogger<Worker> _logger;
 
+        private const string topic = "protocolliIot/drone1/stato";
+        private const string topic_cmd = "protocolliIot/drone1/comando/+";
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
@@ -24,20 +28,90 @@ namespace Protocolli.IoT.Fontana_Scapolan.Worker
         {
             var wb = new WebClient();
             var sensor = new VirtualSensor();
+
+            //Creazione MQTT Client
+            var factory = new MqttFactory();
+            var mqttClient = factory.CreateMqttClient();
+
+            //Utilizzo connessione TCP
+            var options = new MqttClientOptionsBuilder()
+                            .WithTcpServer("192.168.104.86", 1883) //Port is optional
+                             .Build();
+
+            //Consumo dei dati
+            mqttClient.UseApplicationMessageReceivedHandler(e =>
+            {
+                Console.WriteLine("### ESECUZIONE COMANDO: ###");
+                if (Encoding.UTF8.GetString(e.ApplicationMessage.Payload) == "1")//Controllo accensione drone
+                {
+                    if (e.ApplicationMessage.Topic == "protocolliIot/drone1/comando/accensione")
+                    {
+                        Console.WriteLine("Drone acceso");
+                    }
+                    if (e.ApplicationMessage.Topic == "protocolliIot/drone1/comando/led")
+                    {
+                        Console.WriteLine("Led acceso");
+                    }
+                    if (e.ApplicationMessage.Topic == "protocolliIot/drone1/comando/base")
+                    {
+                        Console.WriteLine("Il drone ritorna alla base");
+                    }
+                }
+                else
+                {
+                    if (e.ApplicationMessage.Topic == "protocolliIot/drone1/comando/accensione")
+                    {
+                        Console.WriteLine("Drone spento");
+                    }
+                    if (e.ApplicationMessage.Topic == "protocolliIot/drone1/comando/led")
+                    {
+                        Console.WriteLine("Led spento");
+                    }
+                }
+                Console.WriteLine("");
+            });
+
+
+            mqttClient.UseConnectedHandler(async e =>
+            {
+                Console.WriteLine("### CONNECTED WITH SERVER ###");
+
+                //Sottoscrizione al topic_cmd
+                await mqttClient.SubscribeAsync(new MqttClientSubscribeOptionsBuilder().WithTopicFilter(topic_cmd).Build());
+
+                Console.WriteLine("### SUBSCRIBED ###");
+            });
+
+            //Riconnessione
+            mqttClient.UseDisconnectedHandler(async e =>
+            {
+                Console.WriteLine("### DISCONNECTED FROM SERVER ###");
+                await Task.Delay(TimeSpan.FromSeconds(5));
+
+                try
+                {
+                    await mqttClient.ConnectAsync(options, CancellationToken.None);
+                }
+                catch
+                {
+                    Console.WriteLine("### RECONNECTING FAILED ###");
+                }
+            });
+            await mqttClient.ConnectAsync(options, CancellationToken.None);
+
             while (true)
             {
+                //Ricezione del messaggio
                 var data = sensor.getJson();
-                string url = "https://192.168.104.86:5001/api/Drones"; //Indirizzo IP della macchina su cui gira il server
+                //Pubblicazione del messaggio
+                var message = new MqttApplicationMessageBuilder()
+               .WithTopic(topic)
+               .WithPayload(data)
+               .WithExactlyOnceQoS()
+               .Build();
 
-                var httpClientHandler = new HttpClientHandler();
-                httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) =>
-                {
-                    return true;
-                };
-                using (var client = new HttpClient(httpClientHandler))
-                {
-                    var response = await client.PostAsync(url, new StringContent(data, Encoding.UTF8, "application/json"));
-                }
+                await mqttClient.PublishAsync(message, CancellationToken.None);
+
                 Console.WriteLine(data);
                 System.Threading.Thread.Sleep(20000);
             }
--- README.md
+++ README.md
@@ -5,13 +5,32 @@ COMPONENTI:
 Davide Scapolan = davide.scapolan@stud.tecnicosuperiorekennedy.it
 Marika Fontana = marika.fontana@stud.tecnicosuperiorekennedy.it
 
-SERVER:
-Per avviare correttamente la soluzione del Server è necessario aggiungere una stringa di connessione agli UserSecret dell'API così composti:
+SERVER:Per avviare correttamente la soluzione del Server è necessario aggiungere una stringa di connessione agli UserSecret dell'API così composti:
 
-{
-  "ConnectionStrings": {
-    "Drone": "Server=127.0.0.1;Port=5432;Database=*Nome DB*;User Id=*Nome utente*;Password=*Password utente;"
-  }
-}
+{ "ConnectionStrings": { "Drone": "Server=127.0.0.1;Port=5432;Database=Nome DB;User Id=Nome utente;Password=*Password utente;" } }
 
 N.B. il DB è un DB PostgreSQL fatto girare su Ubuntu montato su WSL2. La connection string andrà modificata nel caso in cui il DB sia montato su un'altra WSL o su un altro sistema operativo
+
+topic:
+	protocolliIot/drone1/stato
+	protocolliIot/drone1/comando/accensione
+	protocolliIot/drone1/comando/led
+	protocolliIot/drone1/comando/base
+
+payload:
+	stato:
+	{
+	  "Id": 0,
+	  "Date": "2021-12-02T15:02:00.364Z",
+	  "Position": "string",
+	  "Speed": 0,
+	  "BatteryLevel": 0,
+	  "IdDrone": 0,
+	  "Time": 0
+	}
+
+	comando/accensione,comando/led:
+	0
+
+	comando/base:
+	1
