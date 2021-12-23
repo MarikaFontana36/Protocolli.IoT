db07278f61fab99dc170a1119defbc7baba482ac
davidescapolan <davide.scapolan@stud.tecnicosuperiorekennedy.it>
2021-12-23 12:58:57 +0100

    rimossa cartella Server perchè rimpiazzata dalla cartella Client

--- Server/ITS.DIQU.FontanaScapolan.ServerDrone/ITS.DIQU.FontanaScapolan.ServerDrone.ApplicationCore/Interfaces/Data/IDronesRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using ITS.DIQU.FontanaScapolan.ServerDrone.ApplicationCore.Entities;
-
-namespace ITS.DIQU.FontanaScapolan.ServerDrone.ApplicationCore.Interfaces.Data
-{
-    public interface IDronesRepository : IRepository<Drone, int>
-    {
-    }
-}
